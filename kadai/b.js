6619d9a815117599749e9ee54ebeda504b33e021
aoaoauto33-maker <kanwaru77@gmail.com>
2026-06-11 08:01:53 +0000

    button mada

--- kadai/b.js
+++ kadai/b.js
@@ -21,10 +21,7 @@ const changeStock ={
 
 
 
-
 // <画面のボタンや入力欄をJavaScriptに持ってくる(決まり文句)>
-const moneyInput = document.getElementById('moneyInput');
-const moneyBtn   = document.getElementById('moneyBtn');
 const itemInput  = document.getElementById('itemInput');
 const itemBtn    = document.getElementById('itemBtn');
 const refundBtn  = document.getElementById('refundBtn');
@@ -38,26 +35,50 @@ let money = 0;
 
 
 // <お金投入>
-moneyBtn.addEventListener('click',() => {
-    const answer = moneyInput.value; //入力欄から文字を受け取る
-    money = Number(answer)
-    // Numberは変換関数 入力された文字列を数値に変換している
+function insertMoney(coin){
+    
+    if(money + coin > 1000){
+        console.log('これ以上は入りません');
 
-if(money > 1000){
-    console.log('もう一度入力してください')
-}else{
-    console.log(`${money}円投入されました`);
+    }else{
+    money += coin;
 
+    changeStock[coin] += 1;
 
-    console.log("商品一覧");
+    console.log(`${coin}円投入されました`);
+    console.log(`現在の投入金額: ${money}円`);
+
+    console.log('商品一覧');
+    console.log('--------');
     for(const  drinkList in drink){
         console.log(`${drinkList}: ${drink[drinkList].price}円`);
     }
+    console.log('--------');
 
+    
     // 隠していた商品エリアを表示させる
     itemArea.style.display = 'block';
+   }
 }
-});
+
+
+
+
+
+// 各ボタンを発火させる
+function btnFire(){
+    const coins = [500,100,50,10]
+    for(const coin of coins){
+        const button = document.getElementById(`btn${coin}`);
+
+        button.addEventListener('click', () => {
+            insertMoney(coin);
+        });
+    }
+}
+btnFire();
+
+
 
 
 
@@ -92,9 +113,10 @@ itemBtn.addEventListener('click', () => {
 
         console.log(`おつり: ${money}円`);
         console.log(`在庫: ${drink[answer2].stock}`);
+
+        algorithm();
     }
 });
-
 
 
 
@@ -109,6 +131,44 @@ refundBtn.addEventListener('click', () =>{
 
     console.log(`${refundMoney}円が返金されました`);
 });
+
+
+
+
+// 釣り銭アルゴリズム
+function algorithm(){
+    let change = money;
+    // 今のmoneyの値を保存しておく
+
+
+    const coins = [500,100,50,10];
+    // 500から順番通りに計算させたいので配列とofを使う
+
+    for(const coinValue of coins){
+
+    // Math.min(必要枚数, 在庫)...少ない方を使うメソッド
+    const coin = Math.min(
+        Math.floor(change / coinValue), 
+        // まずは500で割って何枚になるか計算
+        changeStock[coinValue]
+        // 今抱えている在庫
+    );
+
+    change -= coin * coinValue;
+    // 必要枚数と500円をかけて今の値から引いてそれをchangeに保存
+
+    changeStock[coinValue] -= coin;
+    // 在庫から計算した硬貨の枚数を引く
+
+    console.log(`${coinValue}円玉を${coin}枚数使いました`);
+    console.log(changeStock);
+
+    if(change > 0){
+        console.log('釣銭不足です');
+    }
+
+    }
+}
 
 
 
